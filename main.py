ed8ce6a878bd79724275ad90cd137c3a0bf03cd4
AugmentedR3ality <adv.prg.aug@gmail.com>
2021-03-16 11:04:38 +0100

    Added Fractal-Selection-Menu

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 from simple_term_menu import *
+
+def main():
+    fractals = ["Mandelbrot-Menge", "Julia-Menge", "Sierpinski Dreieck"]
+    fractal_menu = TerminalMenu(fractals, title="Fractal")
+    chosen_fractal = fractal_menu.show()
+    print(chosen_fractal)
+
+
+if __name__=="__main__":
+    main()
