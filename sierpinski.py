fb8dc647e9dfc6642c81a2c38a8a8217538c0fc5
K4ZUN0 <j.jaguttis@gmx.net>
2021-03-17 12:41:56 +0100

    Added menu to sierpinski.

--- sierpinski.py
+++ sierpinski.py
@@ -1,53 +1,85 @@
+import colorsys
 from simple_term_menu import *
 import os
+import random
+import matplotlib.pyplot as plt
+import time
+from tqdm import tqdm
+from PIL import Image
 
 class Sierpinski:
 
 	def __init__(self):
-		print("Sierpinski")
 
-	
-#!/usr/bin/python3
+		invalid = True
+		while invalid:
+			os.system('clear')
+			resolutions = ["Custom resolution", "400 x 400", "800 x 800", "1200 x 1200", "1600, 1600"]
+			tm = TerminalMenu(resolutions, title="Resolution")
+			r = tm.show()
+			if not r == 0:
+				self.width = (r)*400
+				self.height = self.width
+				invalid = False
+			else:
+				os.system('clear')
+				try:
+					self.width = int(input("Resolution (width = height): "))
+					self.height = self.width
+					invalid = False
+				except ValueError:
+					pass
 
-import random
-import matplotlib.pyplot as plt
-import time
+		invalid = True
+		while invalid:
+			os.system('clear')
+			numbers = ["Custom resolution", "10.000", "100.000", "1.000.000", "10.000.000"]
+			tm = TerminalMenu(numbers, title="Iterations")
+			r = tm.show()
+			if not r == 0:
+				self.number = 10**(r+3)
+				invalid = False
+			else:
+				os.system('clear')
+				try:
+					self.number = int(input("Iterations: "))
+					invalid = False
+				except ValueError:
+					pass
 
-t0 = time.time()
+		os.system('clear')
+
+		# self.width = 8000
+		# self.height = 8000
+		self.img = Image.new('RGB', (self.width, self.height), color = 'white')
+		self.pixels = self.img.load()
+
+		self.A = Punkt(0, 0)
+		self.B = Punkt(self.width, 0)
+		self.C = Punkt(self.width/2, self.height)
+		p = Punkt(random.randint(0, self.width), random.randint(0, self.height))
+		# self.number = 1000000000
+		self.algorithm(p)
+		self.img.save('output.png')
+
+	def algorithm(self, p):
+		for i in tqdm(range(self.number)):
+			p = p + (random.choice([self.A, self.B, self.C]) - p) * 0.5
+			self.pixels[p.x,-p.y] = (0, 0, 0)
+
 
 class Punkt():
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-    def __add__(self, x2):
-        r = Punkt(self.x + x2.x, self.y + x2.y)
-        return r
-    def __sub__(self, x2):
-        r = Punkt(self.x - x2.x, self.y - x2.y)
-        return r
-    def __mul__(self, x2):
-        r = Punkt(self.x * x2, self.y * x2)
-        return r
-    def print(self):
-        print(self.x, self.y)
-
-def algorythmus(p):
-    for i in range(number):
-        p = p + (random.choice([A, B, C]) - p) * 0.5
-        plot_x.append(p.x)
-        plot_y.append(p.y)
-
-
-A = Punkt(0, 0)
-B = Punkt(2, 0)
-C = Punkt(1, 2)
-p = Punkt(random.randint(-100, 100), random.randint(-100, 100))
-number = 100000
-plot_x = []
-plot_y = []
-plt.axis([0, 2, 0, 2])
-algorythmus(p)
-plt.plot(plot_x, plot_y, "bo")
-
-print(time.time() - t0)
-plt.show()
+	def __init__(self, x, y):
+        	self.x = x
+        	self.y = y
+	def __add__(self, x2):
+        	r = Punkt(self.x + x2.x, self.y + x2.y)
+        	return r
+	def __sub__(self, x2):
+        	r = Punkt(self.x - x2.x, self.y - x2.y)
+        	return r
+	def __mul__(self, x2):
+        	r = Punkt(self.x * x2, self.y * x2)
+        	return r
+	def print(self):
+        	print(self.x, self.y)
