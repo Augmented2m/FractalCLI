90fb86a4e16539c64af7780bd762835cbe22d3c0
AugmentedR3ality <adv.prg.aug@gmail.com>
2021-03-17 08:16:51 +0100

    Implemented Sierpinski generation

--- sierpinski.py
+++ sierpinski.py
@@ -1,53 +1,50 @@
+import colorsys
 from simple_term_menu import *
 import os
+import random
+import matplotlib.pyplot as plt
+import time
+from tqdm import tqdm
+from PIL import Image
 
 class Sierpinski:
 
 	def __init__(self):
-		print("Sierpinski")
+		os.system('clear')
+		
+		self.width = 8000
+		self.height = 8000
+		self.img = Image.new('RGB', (self.width, self.height), color = 'white')
+		self.pixels = self.img.load()
 
-	
-#!/usr/bin/python3
+		self.A = Punkt(0, 0)
+		self.B = Punkt(self.width, 0)
+		self.C = Punkt(self.width/2, self.height)
+		p = Punkt(random.randint(0, self.width), random.randint(0, self.height))
+		self.number = 1000000000
+		self.algorithm(p)
+		self.img.save('output.png')
 
-import random
-import matplotlib.pyplot as plt
-import time
+	def algorithm(self, p):
+		for i in tqdm(range(self.number)):
+			p = p + (random.choice([self.A, self.B, self.C]) - p) * 0.5
+			self.pixels[p.x,-p.y] = (0, 0, 0)
 
-t0 = time.time()
 
 class Punkt():
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-    def __add__(self, x2):
-        r = Punkt(self.x + x2.x, self.y + x2.y)
-        return r
-    def __sub__(self, x2):
-        r = Punkt(self.x - x2.x, self.y - x2.y)
-        return r
-    def __mul__(self, x2):
-        r = Punkt(self.x * x2, self.y * x2)
-        return r
-    def print(self):
-        print(self.x, self.y)
-
-def algorythmus(p):
-    for i in range(number):
-        p = p + (random.choice([A, B, C]) - p) * 0.5
-        plot_x.append(p.x)
-        plot_y.append(p.y)
+	def __init__(self, x, y):
+        	self.x = x
+        	self.y = y
+	def __add__(self, x2):
+        	r = Punkt(self.x + x2.x, self.y + x2.y)
+        	return r
+	def __sub__(self, x2):
+        	r = Punkt(self.x - x2.x, self.y - x2.y)
+        	return r
+	def __mul__(self, x2):
+        	r = Punkt(self.x * x2, self.y * x2)
+        	return r
+	def print(self):
+        	print(self.x, self.y)
 
 
-A = Punkt(0, 0)
-B = Punkt(2, 0)
-C = Punkt(1, 2)
-p = Punkt(random.randint(-100, 100), random.randint(-100, 100))
-number = 100000
-plot_x = []
-plot_y = []
-plt.axis([0, 2, 0, 2])
-algorythmus(p)
-plt.plot(plot_x, plot_y, "bo")
-
-print(time.time() - t0)
-plt.show()
