3fc10d7eb915690df0335539d9b544be92836616
K4ZUN0 <j.jaguttis@gmx.net>
2021-03-18 15:29:44 +0100

    Fixed indention

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ from sierpinski import Sierpinski
 import os
 
 def main():
-	os.system('clear')
-	fractals = ["Mandelbrot-Menge", "Julia-Menge", "Sierpinski Dreieck"]
-	fractal_class = {0: Mandelbrot, 1: Julia, 2: Sierpinski}
-	fractal_menu = TerminalMenu(fractals, title="Fractal")
-	chosen_fractal = fractal_menu.show()
-	fractal_class[chosen_fractal]()
+    os.system('clear')
+    fractals = ["Mandelbrot-Menge", "Julia-Menge", "Sierpinski Dreieck"]
+    fractal_class = {0: Mandelbrot, 1: Julia, 2: Sierpinski}
+    fractal_menu = TerminalMenu(fractals, title="Fractal")
+    chosen_fractal = fractal_menu.show()
+    fractal_class[chosen_fractal]()
 
 if __name__=="__main__":
-	main()
+    main()
