0428855da514c6d436b12b9c41d3d39069c30690
AugmentedR3ality <adv.prg.aug@gmail.com>
2021-04-07 11:34:52 +0200

    Added acceptable image preview

--- main.py
+++ main.py
@@ -4,12 +4,27 @@ from julia import Julia
 from sierpinski import Sierpinski
 from barnsley import Barnsley
 import os
+from ascii_magic import from_image_file
+
+def show_image_w(x):
+    p = x+'.png'
+    if p in os.listdir():
+        return str(image.Image(p))
+    else:
+        return "No preview available"
+
+def show_image(x):
+    p = x+'.png'
+    if p in os.listdir():
+        return str(from_image_file(p, columns=90))
+    else:
+        return "No preview available"
 
 def main():
     os.system('clear')
     fractals = ["Mandelbrot-Menge", "Julia-Menge", "Sierpinski Dreieck", "Barnsley"]
     fractal_class = {0: Mandelbrot, 1: Julia, 2: Sierpinski, 3: Barnsley}
-    fractal_menu = TerminalMenu(fractals, title="Fractal")
+    fractal_menu = TerminalMenu(fractals, preview_command=show_image, preview_size=1, title="Fractal")
     chosen_fractal = fractal_menu.show()
     fractal_class[chosen_fractal]()
 
